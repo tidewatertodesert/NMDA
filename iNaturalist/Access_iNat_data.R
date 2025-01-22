1ecfa3c3ef57bccfc2f4efab4b72aec8600426e0
tidewatertodesert <DylanBurruss@gmail.com>
2025-01-22 10:50:07 -0700

    Added iNaturalist code
    includes code to access iNaturalist data and produce an interactive widget.

---
+++ iNaturalist/Access_iNat_data.R
@@ -0,0 +1,71 @@
+#useful example: https://cran.r-project.org/web/packages/rinat/vignettes/rinat-intro.html
+
+library(tidyverse)
+library(rinat)
+library(readxl)
+library(httr)
+library(sf)
+library(tmap)
+library(lubridate)
+
+#setwd("C:/Users/dburruss/Documents/R/inaturalist/")
+
+#define region of interests [approx. extent of NM]
+bbox_new_mexico <- c(31.33216, -109.050431, 37.000233, -103.002043)
+
+#####create a loop to download all of the iNaturalist data for listed species
+
+#grab data from github and write to temp dir
+GET("https://github.com/tidewatertodesert/iNaturalist_widget/blob/6fa3b3a5e4c34a4407e4044db9361635c8b75338/Nox_weed_list.xlsx", 
+    write_disk("Nox_weed_list.xlsx", overwrite = TRUE))
+
+list <- read_xlsx("C:/Users/dburruss/Documents/GitHub/NMDA/data/tables/Nox_weed_list.xlsx") %>%
+  as_tibble() %>%
+  mutate(sp_name = gsub(" spp\\.", "",paste0(Genus," ",species))) 
+
+#create blank dataframe
+nox_obs <- tibble()  # Initialize an empty tibble (data frame)
+
+#loop to retrieve all noxious species data
+for (i in list$sp_name) {
+  message("Searching records for: ",i)
+  tryCatch({ #use trycatch to prevent warnings from interrupting download
+    obs <- get_inat_obs(taxon_name = as.character(i), bounds = bbox_new_mexico)
+    nox_obs <- rbind(nox_obs, obs)
+    message(nrow(obs)," records found for ",i)
+    rm(obs)
+  }, error = function(e) {
+    cat("No data found for species:", i, "\n")
+  })
+}
+
+
+
+
+
+inat_obs_sf <- nox_obs %>% #pipes this data through the next 3 lines of code
+  select(longitude, latitude, datetime, common_name, scientific_name, quality_grade) %>% #selects specific vectors [columns of data]
+  mutate(date = date(ymd_hms(datetime, tz="America/Denver"))) %>%
+  st_as_sf(coords=c("longitude", "latitude"), crs=4326) #define coordinate reference system
+
+#Write data 
+write.csv(inat_obs_sf, paste0("C:/Users/dburruss/Documents/GitHub/NMDA/data/tables/inat_obs_",Sys.Date(),".xlsx"))
+
+##### plot the data for a sanity check
+
+# Download New Mexico state boundaries
+usa <- st_as_sf(maps::map("state", fill = TRUE, plot = FALSE)) %>% #grabs a map of US states
+  filter(ID=="new mexico") # filters the polygon to include only rows where column ID == 'new mexico'
+
+# Create a basic plot 
+ggplot(usa) + #uses a widely used plot function
+  geom_sf(color = "#2b2b2b", fill = "white", size = 0.125) + #to plot a shapefile [in this case it is an 'sf' object so we use geom_sf]
+  geom_sf(data=inat_obs_sf, aes(color=scientific_name, shape=quality_grade)) +
+  coord_sf(crs = st_crs("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"), datum = NA) + #defines the coordinate reference system [crs]
+  ggthemes::theme_map() + #provides many canned themes for plot [see https://ggplot2.tidyverse.org/reference/ggtheme.html for more]
+  theme(legend.position = "bottom",
+        legend.key.size = unit(0.1, "cm"), # Reduce legend key size
+        legend.text = element_text(size = 7), # Reduce legend text size
+        legend.box = "horizontal", # Arrange legend items horizontally
+        legend.box.margin = margin(0, 0, 0, 0), # Adjust margin if needed
+        legend.spacing.x = unit(0.1, 'cm')) # Adjust spacing between legend items) #modify plot components here [https://ggplot2.tidyverse.org/reference/theme.html]
---
+++ iNaturalist/Leaflet_SWCD_noxious_weeds.R
@@ -0,0 +1,173 @@
+#leaflet map of observations and ecoregions
+#https://library.virginia.edu/data/articles/data-scientist-as-cartographer-an-introduction-to-making-interactive-maps-in-r-with-leaflet
+#leaflet maps: https://leaflet-extras.github.io/leaflet-providers/preview/
+
+library(leaflet)
+library(magrittr)
+library(sf)
+library(terra)
+library(geojsonio)
+library(htmltools)
+library(htmlwidgets)
+library(stringi)
+library(RColorBrewer)
+library(viridis)
+library(readxl)
+library(tidyverse)
+library(leaflet.extras)
+
+##### Prepare data for the leaflet map
+#read in shapefile of the SWCDs
+SWCDs <- read_sf('C:/Users/dburruss/Documents/GIS/Boundaries/nmswcd/nmswcd.shp') %>%
+  mutate(NAME = ifelse(NAME=="Edgewood", "Tri-County", NAME))
+#st_crs(SWCDs)
+SWCDs <- st_make_valid(st_transform(SWCDs, crs = '+proj=longlat +datum=WGS84')) #convert shapefile to match leaflet map
+#plot(SWCDs)
+
+#Use data from the Access_iNat_data.R script
+inat_obs_sf <- st_make_valid(st_transform(inat_obs_sf, crs = '+proj=longlat +datum=WGS84')) #convert shapefile to match leaflet map
+
+#calculate the percentage of each sp.for all observations within a SWCD
+ggplot() +
+  geom_sf(data=SWCDs)+
+  geom_sf(data=inat_obs_sf, aes(color=scientific_name)) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+#join the SWCD name to the points file
+
+points_SWCD <- st_join(inat_obs_sf, SWCDs, join=st_intersects) %>%
+  filter(!is.na(NAME))
+  
+#calculate the % of each veg type by SWCD
+counts <- points_SWCD %>%
+  group_by(NAME, scientific_name) %>%
+  summarize(count=n())
+
+#calculate the total count of observations and join count data
+totals <- points_SWCD %>%
+  group_by(NAME) %>%
+  summarize(total_count = n()) %>%
+  ungroup() %>%
+  st_join(counts%>%select(-geometry)) %>%
+  mutate(percent = (count / total_count)*100,
+         percent = round(percent, digits=1)) %>%
+  rename(NAME = NAME.x)
+
+totals_table <- totals %>%
+  as_tibble() %>%
+  select(NAME, scientific_name, count, percent) %>%
+  arrange(desc(percent))
+
+#join count/percentages to shapefile and create popup content
+SWCDs_vals <- SWCDs %>%
+  left_join(totals_table) %>%
+  group_by(NAME) %>%
+  summarize(popup_content = paste0("<em>", scientific_name,"</em>",": ", percent, "%", collapse = "<br>")) %>%
+  ungroup() %>%
+  mutate(popup_content = ifelse(grepl("NA", popup_content),"No Records",popup_content))
+
+#fun colors for points
+species_palette <- colorFactor(palette = turbo(n = length(unique(inat_obs_sf$scientific_name))),
+                               domain = inat_obs_sf$scientific_name)
+##### Create custom formating 
+add_custom_layer_control <- function(map) {
+  htmlwidgets::onRender(map, '
+    function(el, x) {
+      var layerControl = document.querySelector(".leaflet-control-layers");
+      if (layerControl) {
+        // Create custom header for base maps
+        var baseMapsHeader = document.createElement("div");
+        baseMapsHeader.className = "custom-layer-control-header";
+        baseMapsHeader.innerHTML = "<strong>Base Maps</strong>";
+        
+        // Insert custom headers into the layer control
+        layerControl.insertBefore(baseMapsHeader, layerControl.firstChild);
+        layerControl.insertBefore(layersHeader, layerControl.firstChild.nextSibling);
+        
+        // Apply custom CSS styling
+        var style = document.createElement("style");
+        style.textContent = `
+          .custom-layer-control-header {
+            #background-color: #ffffff;
+            # padding: 10px;
+            # border-radius: 5px;
+            # border: 1px solid #ccc;
+            # margin-bottom: 10px;
+            font-size: 14px;
+            font-weight: bold;
+          }
+          .leaflet-control-layers {
+            padding: 0;
+          }
+        `;
+        document.head.appendChild(style);
+      }
+    }
+  ')
+}
+  
+#####create a leaflet map
+nox_pcnt <- leaflet() %>% #initializes the map widget
+  addProviderTiles(providers$OpenStreetMap, group = "Open Street Map") %>%
+  addProviderTiles(providers$Esri.WorldImagery, group = "ESRI World Imagery") %>%
+  addProviderTiles(providers$CartoDB.Positron, group = "Simple") %>%
+  addProviderTiles(providers$CyclOSM, group = "CyclOSM") %>%
+  
+  #good but not working
+  #addProviderTiles(provider = "Stadia.AlidadeSmoothDark", group = "Dark") %>%
+  # addProviderTiles(providers$Stadia.StamenTerrain, group = "Terrain") %>%
+  # addProviderTiles(providers$Thunderforest.Landscape, group = "Landscape") %>%
+  
+  
+  #options not used
+  # addProviderTiles(providers$Esri.NatGeoWorldMap, group = "NatGeo") %>%
+  # addProviderTiles(providers$OpenTopoMap, group = "Topo") %>%
+
+  #control layer ordering
+  addMapPane("points", zIndex = 420) %>%
+  
+  addPolygons(data=SWCDs_vals,
+              group="SWCDs",
+              #fillColor = ~colorFactor("viridis", domain=NULL)(NAME),
+              fillColor = ,
+              color = "#ff2f93",
+              dashArray = "3,3",
+              weight = 1,
+              fillOpacity = 0,
+              popup = ~paste(     "<strong>District:</strong>", NAME, "<br>",
+                                  "<br><strong>Record frequency (%)</strong><br>",
+                                  popup_content)
+                
+              ) %>%
+  addCircleMarkers(data=inat_obs_sf,
+                   group="Observations",
+                   radius = 1.55,
+                   color = ~species_palette(scientific_name),  # Apply the color palette
+                   #label = ~District_CWMA,
+                   #clusterOptions=markerClusterOptions(),
+                   options = pathOptions(pane = "points"),
+                   labelOptions = labelOptions(
+                     noHide = FALSE,
+                     textOnly = TRUE,
+                     style = list("color" = "black", "font-weight" = "bold"),
+                     textsize = "10px",
+                     direction = "auto"),
+                   popup = ~paste("<strong>Species:</strong><br><em>", scientific_name, "</em><br>",
+                                  "<strong>Observation Date:</strong><br>", date, "<br>")) %>%
+            
+  addLayersControl(
+    baseGroups = c("Simple","Open Street Map","CyclOSM","ESRI World Imagery"),
+    overlayGroups = c("Observations","SWCDs"),
+    options = layersControlOptions(collapsed = FALSE)) %>%
+  
+  addScaleBar(position = "bottomleft") # Position can be "bottomleft", "bottomright", "topleft", or "topright"
+
+  nox_pcnt <- add_custom_layer_control(nox_pcnt)
+  
+nox_pcnt              
+              
+saveWidget(nox_pcnt, file = 'C:/Users/dburruss/Documents/R/iNat_weed_obs.html')        
+
+
+
