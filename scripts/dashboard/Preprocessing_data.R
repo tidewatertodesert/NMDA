fa8c9981c8c5587f12fa77f6f9339d1b3d036247
tidewatertodesert <DylanBurruss@gmail.com>
2025-04-11 10:53:56 -0600

    Updated code for preprocessing data and summary figures for NW sampling.

---
+++ scripts/dashboard/Preprocessing_data.R
@@ -0,0 +1,164 @@
+#Process manually downloaded data for dashboard construction
+library(tidyverse)
+library(tidyterra)
+library(terra)
+library(sf)
+library(stars)
+library(tigris)
+library(lubridate)
+
+#Run this after the EDDMapS_api.R script
+####grab the shapefiles
+
+#point data
+points <- st_read("EDDMapS/EDDMapS_raw/points_raw.shp")
+message(nrow(points)," features in points layer and ", length(unique(points$scntfcn)), " unique species.")
+#names(points)
+
+#simple polygons
+polygons <- st_read("EDDMapS/EDDMapS_raw/polygon_raw.shp") %>%
+  st_make_valid()
+message(nrow(polygons)," features in combined polygon layer and ", length(unique(polygons$scntfcn)), " unique species")
+
+message("Total records: ",nrow(points)+nrow(polygons))
+
+#####select columns to keep from records
+# columns <- c("scientificname",
+#              "displayname",
+#              "coordinates",
+#              "geogtype",
+#              "geogwkt",
+#              "observationdate",
+#              "infestationstatus",
+#              "eradicationstatus",
+#              "infestedarea",
+#              "infestedareaunits",
+#              "reporter")
+
+columns <- c("scntfcn",
+             "dsplynm",
+             "cordnts",
+             "geogtyp",
+             "geogwkt",
+             "obsrvtndn",
+             "infsttn",
+             "erdctns",
+             "infestdr",
+             "infstdrn",
+             "reportr")
+
+
+#select columns from point data
+points_sl <- points %>%
+  dplyr::select(all_of(columns))
+
+#select columns from combined polygon data
+polygons_sl <- polygons %>%
+  select(all_of(columns))
+
+#convert polygon centroids to points (dashboard summaries only use point data)
+centrd <- polygons_sl %>%
+  st_cast("POLYGON") %>% #split multipart polygons into separate polygons
+  st_centroid()
+
+#add centroids to points layer
+points_comb <- points_sl %>%
+  rbind(centrd)
+message("Total records: ",nrow(points_comb),". Unique species: ", length(unique(points_comb$scntfcn)))
+
+####bring in state county and swcd data to add column to each shapefile
+#read in shapefile of the SWCDs
+SWCD <- st_read("data/shapefiles/nmswcd/nmswcd.shp") %>%
+  st_make_valid(SWCD) %>% # fix invalid geometries
+  mutate(NAME = ifelse(NAME=="Edgewood", "Tri-County", NAME)) %>%
+  st_transform(crs=st_crs(points)) %>%
+  select(NAME, Abbr) %>%
+  rename(SWCD_name = NAME,
+         SWCD_abbr = Abbr) 
+  
+#identical(st_crs(points), st_crs(SWCD)) #test if crs match
+
+#grab NM county boundaries
+nm_COUNTY <- counties(state = "NM", cb = TRUE) %>%  # cb = TRUE for simplified geometries
+  st_transform(crs=st_crs(points)) %>%
+  select(NAME, NAMELSAD) %>%
+  rename(County = NAME,
+         County_lsad = NAMELSAD)
+
+#identical(st_crs(points), st_crs(nm_COUNTY)) #test if crs match
+names(nm_COUNTY)
+
+#### Compute the county and swcd intersect for points and polygon layers
+points_join <- points_comb %>% 
+  st_join(nm_COUNTY) %>%
+  st_join(SWCD) 
+
+#### Compute the county and swcd intersect for polygon layer
+polygon_int <- polygons_sl %>%
+  st_intersection(SWCD) %>%
+  st_intersection(nm_COUNTY)
+
+#write out the data
+st_write(points_join, "EDDMapS/Processed_data/points_clean.shp",
+         append=FALSE, delete_layer=TRUE)
+
+length(unique(points_join$scntfcn))
+
+#write out the data
+st_write(polygon_int, "EDDMapS/Processed_data/polygon_clean.shp",
+         append=FALSE, delete_layer=TRUE)
+
+#write out the data
+st_write(SWCD, "data/shapefiles/nmswcd/SWCD_updated.shp",
+         append=FALSE, delete_layer=TRUE) 
+
+# #change the format of columns to reflect data
+# points_join <- points_join %>%
+#   mutate(ObsDate = mdy(obsrvtndn),
+#          Year = year(ObsDate),
+#          Abundance = as.numeric(abundnc),
+#          InfestAcre = as.numeric(InfestAcre),
+#          GrossAcre = as.numeric(grossar),
+#          Percentcov = as.numeric(prcntcv),
+#          Density = as.numeric(density),
+#          Quantity = as.numeric(quantty),
+#          QuantityU = as.numeric(qnttynt))
+# 
+# polygon_int <- polygon_int %>%
+#   mutate(ObsDate = mdy(ObsDate),
+#          Year = year(ObsDate),
+#          Abundance = as.numeric(Abundance),
+#          InfestAcre = as.numeric(InfestAcre),
+#          GrossAcre = as.numeric(GrossAcre),
+#          Percentcov = as.numeric(Percentcov),
+#          Density = as.numeric(Density),
+#          Quantity = as.numeric(Quantity),
+#          QuantityU = as.numeric(QuantityU),
+#          APPXQuant = as.numeric(APPXQuant))
+# 
+# #Add polygon centroids to points and append to points dataframe (used for calculating fields in dashboard)
+# centrds <- polygon_int %>%
+#   st_point_on_surface()
+# 
+# #combine points
+# points_join <- points_join %>%
+#   rbind(centrds)
+# 
+# #read in NLCD data
+# nlcd <- rast("C:/Users/dburruss/Documents/GIS/nlcd_conus_2019_prj/nlcd_2019_prj.img")
+# 
+# #grab NLCD lookup table
+# nlcd_tab <- read.csv("C:/Users/dburruss/Documents/GIS/nlcd_conus_2019_prj/nlcd_lookup_table.csv") %>%
+#   as_tibble() %>%
+#   mutate(NLCD_Value = as.factor(NLCD_Value))
+# 
+# # Make sure both CRS are the same, if not transform
+# identical(crs(nlcd), st_crs(points_join))
+# 
+# 
+# points_ext <- terra::extract(nlcd, points_join) %>%
+#   cbind(points_join) 
+# 
+# points_join <- points_ext %>%
+#   left_join(nlcd_tab, by=c("Layer_1"="NLCD_Value"))
+
